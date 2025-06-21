4336323cf348121b144ed95d5ddb2125f3c10973
Mahmoud <mahmoudabdelsalam.alpha@gmail.com>
2025-06-21 19:56:25 +0300

    first commit

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://192.168.1.4:5001/api"
+      : "/api",
+  withCredentials: true,
+});
